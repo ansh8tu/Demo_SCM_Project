072d9bf309526473528e9f03be5cba0bc91c5560
AkshatMishra01 <mishraakshat924@gmail.com>
2020-03-26 18:30:52 +0530

    commit1

--- main.h
+++ main.h
@@ -2,18 +2,20 @@
 using namespace std;
 
 int add(int x, int y);
+int substract(int x,int y);
 int main()
 {
     int number_1; 
     int number_2; 
     int sum_of_numbers;
+    int diff_of_numbers;
    
     cout << "Enter the first number" << endl;
     cin>>number_1;
     cout << "Enter the second number" << endl;
     cin>>number_2;
     sum_of_numbers=add(number_1, number_2);
-  
+    diff_of_numbers=substract(number_1, number_2);
     cout<<"sum of two numbers "<<sum_of_numbers<<endl;
     return 0;
 }
@@ -23,5 +25,12 @@ int add(int x, int y)
 {
     return (x+y);
 }
-
+int substract(int x, int y)
+{
+	
+	if (x>y)
+	return (x-y);
+	if (y>x)
+	return (y-x);
+}
 
