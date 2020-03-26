36e1d408a36dc92d213be9afa03fe5ecc87a033c
animesh-deka <animesh7637@gmail.com>
2020-03-26 19:15:12 +0530

    Animesh: Added multiply function in main.h

--- main.h
+++ main.h
@@ -2,19 +2,22 @@
 using namespace std;
 
 int add(int x, int y);
+int multiply(int x, int y);
 int main()
 {
     int number_1; 
     int number_2; 
     int sum_of_numbers;
+    int product_of_numbers;
    
     cout << "Enter the first number" << endl;
     cin>>number_1;
     cout << "Enter the second number" << endl;
     cin>>number_2;
     sum_of_numbers=add(number_1, number_2);
-  
+    product_of_numbers=multiply(number_1, number_2);
     cout<<"sum of two numbers "<<sum_of_numbers<<endl;
+    cout<<"product of two numbers"<<product_of_numbers<<end1;
     return 0;
 }
 
@@ -22,6 +25,10 @@ int main()
 int add(int x, int y)
 {
     return (x+y);
+}
+int multiply(int x, int y)
+{
+	return (x*y);
 }
 
 
